5f62311709dec9e4f765dac33b95b7aed0e573ee
Ishara Ekanayaka <isharaekanayaka24@gmail.com>
2023-05-16 05:42:32 +0530

    add Multiple screen

--- main.py
+++ main.py
@@ -2,26 +2,42 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.core.audio import SoundLoader
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 # The Widget class is the base class required for creating Widgets
 from kivy.uix.widget import Widget
-# to change the kivy default settings we use this module config
-from kivy.config import Config
-Config.set('graphics', 'resizable', True)
-class MyApp(App):
-    def build(self):
-        music = SoundLoader.load("audio.mp3")
-        if music:
-            music.play()
-        self.img = Image(source ='R.png')
-        self.img.allow_stretch = True
-        self.img.keep_ratio = False
-        self.img.size_hint_x = 1
-        self.img.size_hint_y = 1
-        self.img.pos = (350, 400)
-        self.img.opacity = 1
-        s = Widget()
-        s.add_widget(self.img)
-        return s
+
+class Dashboard(Screen):
+    pass
+
+
+class PianoTilesWindow(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.piano = SoundLoader.load('audio.mp3')
+  
+    def play_piano(self):
+        if self.piano:
+            self.piano.play()
+
+    def stop_piano(self):
+        self.piano.stop()
+
+class TicTacToeWindow(Screen):
+    pass
+
+class HangmanLiteWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class GameArcadeApp(App):
+    # def build(self):
+    #     music = SoundLoader.load("audio.mp3")
+    #     if music:
+    #         music.play()
+    pass
+        
 # run the App
 if __name__ == '__main__':
-    MyApp().run()
+    GameArcadeApp().run()
